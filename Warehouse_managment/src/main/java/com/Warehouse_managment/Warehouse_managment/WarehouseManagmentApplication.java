813b1cce60ad1650fc1b3edbdff2e011963a281c
a1bertnguyen <nqminh.gdsciu@gmail.com>
2025-09-26 22:35:14 +0700

    Stucture the project

---
+++ Warehouse_managment/src/main/java/com/Warehouse_managment/Warehouse_managment/WarehouseManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.Warehouse_managment.Warehouse_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManagmentApplication.class, args);
+	}
+
+}
---
+++ Warehouse_managment/src/test/java/com/Warehouse_managment/Warehouse_managment/WarehouseManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Warehouse_managment.Warehouse_managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7f9720a924d246c83ea1c1706c29ea20160f8eeb
OshiroDrey <minhtoan2746@gmail.com>
2025-11-14 09:07:15 +0700

    add response classes

---
+++ Warehouse_managment/src/main/java/com/Warehouse_managment/Warehouse_managment/Response/LoginRequest.java
@@ -0,0 +1,19 @@
+package com.Warehouse_managment.Warehouse_managment.Response;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}

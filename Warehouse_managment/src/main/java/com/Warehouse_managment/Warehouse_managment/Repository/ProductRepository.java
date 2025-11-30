08d7586f89dd21b2ff045c4a9ad2e051e1979c62
Nguyen Quang Minh <nqminh.gdsciu@gmail.com>
2025-11-30 23:02:27 +0700

    debugg feature product Adding experyDate and sorted by exparyDate

--- Warehouse_managment/src/main/java/com/Warehouse_managment/Warehouse_managment/Repository/ProductRepository.java
+++ Warehouse_managment/src/main/java/com/Warehouse_managment/Warehouse_managment/Repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.Warehouse_managment.Warehouse_managment.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
--- Warehouse_managment/src/main/java/com/Warehouse_managment/Warehouse_managment/Service/ProductService.java
+++ Warehouse_managment/src/main/java/com/Warehouse_managment/Warehouse_managment/Service/ProductService.java
@@ -4,6 +4,7 @@ import com.Warehouse_managment.Warehouse_managment.Dtos.ProductDTO;
 import com.Warehouse_managment.Warehouse_managment.Dtos.Response;
 import org.springframework.web.multipart.MultipartFile;
 
+
 public interface ProductService {
     Response saveProduct(ProductDTO productDTO, MultipartFile imageFile);
 
@@ -16,4 +17,7 @@ public interface ProductService {
     Response deleteProduct(Long id);
 
     Response searchProduct(String input);
+
+
+
 }
